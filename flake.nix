db951dfccd07e5ca2c5f2e61eeb808d5bacfe2c9
sobte <i@sobte.me>
2026-03-21 01:42:39 +0800

    feat: update flake

--- flake.nix
+++ flake.nix
@@ -39,6 +39,6 @@
         permittedInsecurePackages = [ ];
       };
 
-      outputs-builder = channels: { formatter = channels.nixpkgs.nixfmt-rfc-style; };
+      outputs-builder = channels: { formatter = channels.nixpkgs.nixfmt; };
     };
 }
